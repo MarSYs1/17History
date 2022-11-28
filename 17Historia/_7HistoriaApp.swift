797bf5d8ac8a9d2154a4f682724c4d619906195b
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-11-28 13:01:59 -0800

    Initial Commit

---
+++ 17Historia/_7HistoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7HistoriaApp.swift
+//  17Historia
+//
+//  Created by Mauro de Oliveira  Andre on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct _7HistoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 17HistoriaUITests/_7HistoriaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _7HistoriaUITestsLaunchTests.swift
+//  17HistoriaUITests
+//
+//  Created by Mauro de Oliveira  Andre on 28/11/22.
+//
+
+import XCTest
+
+final class _7HistoriaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
